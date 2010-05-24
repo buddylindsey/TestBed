2dc5beef209aa2a531ca9a69ef2212120281e751
unknown <Buddy@.(none)>
2010-05-24 02:54:01 -0500

    Added Euler problems 1 and 2 solutions

---
+++ NET/EulerProject/EulerProblem1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EulerProblem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> mainList = PopulateList();
+            List<double> numbersToAdd = AllNumbersToAdd(mainList);
+            double total = AddAllNumbers(numbersToAdd);
+
+            Console.WriteLine(total);
+        }
+
+        private static double AddAllNumbers(List<double> numbersToAdd)
+        {
+            double final = 0.0;
+
+            foreach (var n in numbersToAdd)
+            {
+                final += n;
+            }
+
+            return final;
+        }
+
+        private static List<double> AllNumbersToAdd(List<double> mainList)
+        {
+            List<double> numbersToAdd = new List<double>();
+
+            foreach (var n in mainList)
+            {
+                if ((n % 3) == 0 || (n % 5) == 0)
+                {
+                    numbersToAdd.Add(n);
+                }
+            }
+
+            return numbersToAdd;
+        }
+
+        private static List<double> PopulateList()
+        {
+            List<double> mainList = new List<double>();
+
+            for (double i = 1.0; i < 1000.0; i++)
+            {
+                mainList.Add(i);
+            }
+
+            return mainList;
+        }
+    }
+}
---
+++ NET/EulerProject/EulerProblem2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EulerProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i1 = 0;
+            int i2 = 1;
+            int total = 0;
+            int total2 = 0;
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; total < 3524578; i++)
+            {
+                total = i1 + i2;
+                i1 = i2;
+                i2 = total;
+
+                DeterminEven(numbers, total);
+
+                Console.WriteLine(total);
+            }
+
+            Console.WriteLine(numbers.Count);
+            CountThemAll(numbers);
+        }
+
+        private static void CountThemAll(List<int> numbers)
+        {
+            int total = 0;
+
+            foreach (int n in numbers)
+            {
+                //Console.WriteLine(n);
+                total += n;
+            }
+
+            Console.WriteLine(total);
+
+        }
+
+        private static void DeterminEven(List<int> numbers, int total)
+        {
+            if ((total % 2) == 0)
+                numbers.Add(total);
+        }
+    }
+}
