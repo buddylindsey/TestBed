f266022b19d8af8167e2d055516a28dcf4cbf509
unknown <Buddy@.(none)>
2010-05-28 00:15:05 -0500

    changed remote file location

--- powershell/processCsvFiles.ps1
+++ powershell/processCsvFiles.ps1
@@ -2,11 +2,11 @@
 foreach($file in $files)
 {
 
-	copy $file.fullname "\\Intranet-server\Techfiles\SUPPORT\Qwest\data\temp"
+	copy $file.fullname "\\server\temp"
 	
 	sc $file.fullname $( gc $file.fullname | ?{$_[0] -ne " " -and $_[0] -ne "N" -and $_[0] -ne "D" -and $_.length -gt 0} )
 	
-	copy $file.fullname \\Intranet-server\Techfiles\SUPPORT\Qwest\data\ReadyFiles
+	copy $file.fullname \\server\ReadyFiles
 
 	write-output $file.fullname
 }
