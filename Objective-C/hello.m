fe2dd043b80dfa5c3303f0fb423efa725c46f158
Buddy Lindsey <percent20@gmail.com>
2010-07-08 11:12:25 -0500

    added line in comment for how to compile ObjC in CLI

--- Objective-C/hello.m
+++ Objective-C/hello.m
@@ -1,6 +1,6 @@
 // Hello World Application
 // Buddy Lindsey - 7/8/2010
-
+// compile with "gcc hello.m -o <outputName> -ObjC"
 #import "Foundation/foundation.h"
 
 int main (int argc, char const *argv[])
